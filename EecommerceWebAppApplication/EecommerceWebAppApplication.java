3a3549f85f8672dfe35b4145ba9be893f23c0531
chandankumar1107 <156430461+ChandanChand1107@users.noreply.github.com>
2024-01-15 13:09:07 +0530

    Add files via upload

---
+++ EecommerceWebAppApplication/EecommerceWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.zosh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EecommerceWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EecommerceWebAppApplication.class, args);
+	}
+
+}
